256ec642494c25e9e0001a76b52ae2772bb65330
Dario <dario.valastro@gmail.com>
2022-07-07 12:25:13 +0200

    Input in Sprint 3

---
+++ scripts/sprint3.js
@@ -0,0 +1,17 @@
+'use strict';
+//Non obbligatorio usare questo listener
+/*
+document.addEventListener("DOMContentLoaded", async function(event) {
+    let j = 0;
+    for (let key in db.marketingMix) {
+        j=j+1;
+        //console.log(key);
+        //onsole.log(db.marketingMix[key].input);
+        document.getElementById("Input_MM".concat(j.toString())).innerHTML = db.marketingMix[key].input;
+    }
+});*/
+
+
+function correggi() {
+    console.log("Pulisci");
+}
